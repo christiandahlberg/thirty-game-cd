5cd243897ab994a04c1d5ac37c90c43e4c5864eb
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-06-26 18:07:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# thirty-game-cd
---
+++ app/src/androidTest/java/chdah/umu/thirthy_game_cd/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package chdah.umu.thirthy_game_cd;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("chdah.umu.thirthy_game_cd", appContext.getPackageName());
+    }
+}
