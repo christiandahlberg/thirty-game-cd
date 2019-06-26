4a780670eed51561da937dac1455ba3e5c9a5334
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-06-26 22:55:43 +0200

    Dice.java completed.

--- app/src/main/java/chdah/umu/thirthy_game_cd/view/Dice.java
+++ app/src/main/java/chdah/umu/thirthy_game_cd/view/Dice.java
@@ -13,32 +13,53 @@ package chdah.umu.thirthy_game_cd.view;
 import android.widget.ImageButton;
 
 /**
- * Resembles every dice and it's image in the game. Sets an image of a dice depending on the value.
+ * Resembles every dice and it's image in the game (the ImageButton-view components themselves).
+ * Sets an image of a dice depending on the value.
  * Also checking whether or not the dice is available to roll (if user has selected it or not).
  */
 public class Dice {
     private ImageButton mDice;
     private Boolean isAvailable;
 
+    /**
+     * Constructor for a Dice
+     * @param ib sets the Dice-values to be used in later stages.
+     */
     public Dice (ImageButton ib) {
-        isAvailable = true;
-        mDice = ib;
+        this.setIsAvailable(true);
+        this.setDiceButton(ib);
     }
 
     // SET / GET
 
+    /**
+     *  Links the argument ImageButton to this Dice's ImageButton field.
+     * @param ib resembles an object with specific data to be set to this class' field.
+     */
     public void setDiceButton(ImageButton ib) {
         mDice = ib;
     }
 
+    /**
+     * Returns specific Dice (ImageButton)
+     * @return
+     */
     public ImageButton getDiceButton () {
         return mDice;
     }
 
+    /**
+     * Determines whether or not the Dice object should be available for re-rolling or not.
+     * @param b is either True or False, depending on usage, which sets the availability.
+     */
     public void setIsAvailable (Boolean b) {
         isAvailable = b;
     }
 
+    /**
+     * Returns whether or not the Dice object is available for re-rolling or not.
+     * @return
+     */
     public Boolean getIsAvailable () {
         return isAvailable;
     }
